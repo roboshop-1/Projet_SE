775f9f8a432ef8b46fcd5f860363fdb5a0340232
Ghofran Ben Tkhayat <105112618+roboshop-1@users.noreply.github.com>
2022-12-15 21:20:14 +0100

    Update code.c

--- code.c
+++ code.c
@@ -61,7 +61,7 @@ void ajout_environnement(char *nom_variable,char *valeur_variable)
 		}
 	}
 }
-//----- fonction remplacer une chaine par une autre chaine 
+
 void str_replace(char *chaine,char* recherche,char *remplace)
 {
 int nbre=0;
@@ -101,7 +101,9 @@ int  boucle,increment,longueur;
 	{
 		arguments[boucle]=NULL;
 	}
-	longueur=strcspn(commande," \"");
+	
+	longueur=strcspn(commande," \0");
+	//printf("----------longueur du comm--%d---\n",longueur);
 	arguments[0]=strndup(commande,longueur);
 	commande=commande+longueur;
 	increment=1;
@@ -117,6 +119,8 @@ int  boucle,increment,longueur;
 		if (strcmp(separateur,"\"")==0) ++commande;
 		++increment;
 	}
+	//printf("----------commande--%d---\n",*commande);
+	//printf("----------arguments--%s---\n",*arguments);
 }
 
 void liberation_arguments(char *arguments[32])
@@ -168,7 +172,7 @@ int  increment=0;
 			arguments[increment]=NULL;
 		}
 		++increment;
-		
+		//printf("----------redirection--%s---\n",redirection);
 	}
 	return redirection;
 }
@@ -178,97 +182,79 @@ void removeWhiteSpace(char* buf){
 	buf[strlen(buf)-1]='\0';
 	if(buf[0]==' ' || buf[0]=='\n') memmove(buf, buf+1, strlen(buf));
 }
-//----------------------------------------------
-void tokenize_buffer(char** param,int *nr,char *buf,const char *c){
-	char *token;
-	token=strtok(buf,c);
-	int pc=-1;
-	while(token){
-		param[++pc]=malloc(sizeof(token)+1);
-		strcpy(param[pc],token);
-		
-	
-		removeWhiteSpace(param[pc]);
-		token=strtok(NULL,c);
-	}
-	param[++pc]=NULL;
-	*nr=pc;
-}
-//------------------------------------------
-void executeAsync(char** buf,int nr){
-	int i,pc;
-	char *argv[100];
-	for(i=0;i<nr;i++){
-		tokenize_buffer(argv,&pc,buf[i]," ");
-		if(fork()==0){
-			execvp(argv[0],argv);
-			perror("invalid input");
-			exit(1);//in case exec is not successfull, exit
-		}
-	} 
-	for(i=0;i<nr;i++){
-		wait(NULL);
-	}
+//*********************************************/
+int executeOR ( char *cmd1 ){
+int pipefd[2];
+creation_liste_arguments(arg_list,cmd1);	
 
+pipe(pipefd);
+pid_t process=fork();
+if (process==0){
+
+int retour=execvp(arg_list[0],arg_list);
+if (retour==-1) { 
+//fprintf(stderr,"%s\n",strerror(errno));
+return retour;}
+exit(1);
+return retour ;
 }
-//------------------------------------------------
-void traitement_cmd(char *commande,char **argv)
-{
-char *cmd1,*cmd2;
-int nr=0;
-char buf[500], *buffer[100];
+  else
+  {
+  wait(&process);
+  }
+}
+//----------------------------------------
+void executeAsync ( char *cmd1 ){
+
+ int pipefd[2];
+ 
+  creation_liste_arguments(arg_list,cmd1);	
+  pipe(pipefd);
+  pid_t process=fork();
+  
+  if (process==0)
+  {
+   execvp(arg_list[0],arg_list) ;
+   perror("invalid input");
+   exit(1);
+  }
+  else
+  {
+  wait(&process);
+  }
+}
+//-----------------------------
+
+//---------------------------
+void executePipe ( char *cmd1 , char *cmd2 ){
 char *fichier_redirection_sortante;
 char *fichier_redirection_sortante2;
-int pipefd[2];
-
-	cmd2=NULL;
-	str_replace(commande,"\n","");
-	str_replace(commande," >",">");
-	str_replace(commande,"> ",">");
-	str_replace(commande,">"," > ");
-	str_replace(commande,"| ","|");
-	str_replace(commande," |","|");
-	str_replace(commande,"|"," | ");
-	char *tmp=strstr(commande," | ");
-	/*------------------------------*/
-	str_replace(commande,"&& ","&&");
-	str_replace(commande," &&","&&");
-	str_replace(commande,"&&"," && ");
-	char *tmp1=strstr(commande," && ");
-	/**---------------------------------*/
-	if (tmp!=NULL) 
-	{
-		cmd1=strndup(commande,strlen(commande)-strlen(tmp));
-		cmd2=strdup(tmp+3);
-		
-	}
-	else
-	{
-		cmd1=strdup(commande);
-	}
-	creation_liste_arguments(arg_list,cmd1);
-	
-	
-	
-	fichier_redirection_sortante=scan_redirection_sortante(arg_list);
-	
-	if (cmd2!=NULL)
+ int pipefd[2];
+ 
+ creation_liste_arguments(arg_list,cmd1);
+ fichier_redirection_sortante=scan_redirection_sortante(arg_list);	
+ if (cmd2!=NULL)
 	{
 		creation_liste_arguments(arg_list2,cmd2);
-		
-		
 		fichier_redirection_sortante2=scan_redirection_sortante(arg_list2);
-	}		
-	pipe(pipefd);
+		
+	}
+
+pipe(pipefd);
+		//The pid_t data type is a signed integer type which is capable of representing a process ID
 	pid_t process=fork();
+	
+	// child process
 	if (process==0)
 	{
 		if (cmd2!=NULL) 
 		{
+		
 			dup2(pipefd[1],STDOUT_FILENO);
 		}
 		if (fichier_redirection_sortante!=NULL)
 		{
+		
 			char* type_redirection=strndup(fichier_redirection_sortante,1);
 			int handler=(intptr_t)freopen(fichier_redirection_sortante+1,type_redirection, stdout);
 			if (handler==-1) 
@@ -278,6 +264,7 @@ int pipefd[2];
 			}
 			free(type_redirection);
 		}
+		// printf("---arg_list[0]  %s \n",*arg_list);
 		int retour=execvp(arg_list[0],arg_list);
 		if (retour==-1) fprintf(stderr,"%s\n",strerror(errno));
 		exit(0);
@@ -288,6 +275,7 @@ int pipefd[2];
 	}
 	if (cmd2!=NULL)
 	{
+	//The pid_t data type is a signed integer type which is capable of representing a process ID
 		pid_t process2=fork();
 		if (process2==0)
 		{
@@ -323,7 +311,74 @@ int pipefd[2];
 	if (fichier_redirection_sortante!=NULL) free(fichier_redirection_sortante);
 	liberation_arguments(arg_list);
 	free(cmd1);
+}
+
+//------------------------------------------------
+void traitement_cmd(char *commande,char **argv)
+{
+char *cmd1,*cmd2;
+int nr=0;
+char buf[500], *buffer[100];
+char *fichier_redirection_sortante;
+char *fichier_redirection_sortante2;
+int pipefd[2];
+
+	cmd2=NULL;
+	str_replace(commande,"\n","");
+	str_replace(commande," >",">");
+	str_replace(commande,"> ",">");
+	str_replace(commande,">"," > ");
+	str_replace(commande,"| ","|");
+	str_replace(commande," |","|");
+	str_replace(commande,"|"," | ");
+	char *tmp=strstr(commande," | ");
+	/*------------------------------*/
+	str_replace(commande,"&& ","&&");
+	str_replace(commande," &&","&&");
+	str_replace(commande,"&&"," && ");
+	char *tmp1=strstr(commande," && ");
+	/*------------------------------*/
+	str_replace(commande,"or ","or");
+	str_replace(commande," or","or");
+	str_replace(commande,"or"," or ");
+	char *tmp2=strstr(commande," or ");
+	if (tmp2!=NULL) {
+		cmd1=strndup(commande,strlen(commande)-strlen(tmp2));
+		cmd2=strdup(tmp2+4);
+		
+	}
 	
+	else if (tmp1!=NULL) {
+		cmd1=strndup(commande,strlen(commande)-strlen(tmp1));
+		cmd2=strdup(tmp1+4);
+	}
+	
+	else if (tmp!=NULL) 
+	{
+		cmd1=strndup(commande,strlen(commande)-strlen(tmp));
+		cmd2=strdup(tmp+3);
+		
+	}
+	else
+	{
+		cmd1=strdup(commande);
+		executeAsync (cmd1 )   ;
+		
+	}
+	if (tmp1!=NULL) {
+	  executeAsync (cmd1 )   ;
+	  executeAsync (cmd2 )   ;
+	}
+	
+	if (tmp2!=NULL) {
+	int a = executeOR (cmd1 );
+	if (a==-1) {
+	executeOR (cmd2 );
+	}
+	}
+	if (tmp!=NULL) {
+	  executePipe (cmd1 ,cmd2 )   ;
+	}
 }
 // elimination des espaces au debut de commande
 void traitement_espaces_debut(char *chaine_a_traiter)
@@ -348,12 +403,10 @@ void traitement_ligne(char **argv)
 {
 	traitement_espaces_debut(buffer);
 	traitement_espaces_fin(buffer);
-	if (buffer[0]=='#')
-	{	
-	}
+	
 	// fonctionnement de commande cd 
 	// 
-	else if (strncmp(buffer,"cd",2)==0)
+	if (strncmp(buffer,"cd",2)==0)
 	{		
 		char *chemin=strstr(buffer," ");
 		int longueur_chemin=strcspn(chemin+1," ");
@@ -364,7 +417,7 @@ void traitement_ligne(char **argv)
 		{
 			fprintf(stderr,"cd : %s",strerror(errno));
 		}
-		// else na5dho pwd legdim z nzidoloo esm dossier
+		// else na5dho pwd legdim w nzidoloo esm dossier
 		else
 		{
 			char *ancien_chemin=getenv("PWD");
@@ -420,7 +473,7 @@ void traitement_ligne(char **argv)
 		free(cmd);
 	}
 }
-//---- lecture derniere commande mel history kif na3mlo touche haute  sinon nulle
+
 int touche_fleche_haute()
 {
 	HIST_ENTRY *historique=history_get(history_length);
@@ -428,7 +481,7 @@ int touche_fleche_haute()
 	rl_end_of_line(0,0);
 	return 0;
 }
-/* zayda**--- ka option **-
+
 int touche_tab()
 {
 	char *buffer=strdup(rl_line_buffer);
@@ -457,9 +510,9 @@ int touche_tab()
 	free(buffer);
 	return 0;
 }
-*/
 
-// affichage de promt --> nom directory + %
+
+// permier promt .
 char *lecture()
 {
 char *tmp=NULL;
@@ -513,7 +566,7 @@ int main(int argc,char *argv[],char *arge[])
 	read_history(".myshel_history");
 	stifle_history(500);
 	write_history(".myshel_history");
-	welcomeScreen();
+	 //welcomeScreen();
 	int increment=0;
 	while (arge[increment]!=NULL)
 	{
@@ -539,10 +592,10 @@ int main(int argc,char *argv[],char *arge[])
 	// STDIN ->It's an I/O stream, basically an operating-system level abstraction that allows data to be read 
 //	(or written, in the case off stdout).
 	else fichier=stdin;
-// lecture  des  promt
+
 	while(1)
 	{
-	
+	// lecture premiere promt
 		char *ligne_saisie=lecture();
 		if (ligne_saisie!=NULL)
 		{
